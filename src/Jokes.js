ecd716530f39049b1bb27536a3fc65e272ff60eb
anjalipoudel <sabnom77poudel@gmail.com>
2023-03-22 22:57:35 +0545

    init

--- src/Jokes.js
+++ src/Jokes.js
@@ -6,6 +6,11 @@ import React, { useState, useEffect } from 'react'
 export default function Jokes() {
     // const [category, setCategory] = useState("");
     const [quizes, setQuizes] = useState([]);
+    const [answer, setAnswer] = useState("")
+
+
+
+
 
     useEffect(() => {
         const url = "https://the-trivia-api.com/api/questions/";
@@ -25,16 +30,49 @@ export default function Jokes() {
 
     }, []);
 
+    if (setAnswer === { quiz["correctAnswer"]}) {
+
+    }
+
+
+
+
     return (
-        <>{
-            quizes.map((quiz, id) => {
-                return (
-                    <div key={id}>
-                        {quiz['category']}
-                    </div>
-                )
-            })
-        },
+        <>
+            {
+                quizes.map((quiz, id) => {
+                    return (
+                        <div key={id} style={{ backgroundColor: "pink", width: "500px", height: "200px", margin: "20px" }} >
+
+                            <div style={{ color: "blue", font: "400" }}> {quiz['question']}</div>
+                            <div style={{ display: "flex", flexDirection: "column", }} >
+                                <ol style={{ listStyleType: "lower-alpha" }}>
+                                    <li>{quiz["incorrectAnswers"][0]}</li>
+                                    <li>{quiz["incorrectAnswers"][1]}</li>
+                                    <li>{quiz["incorrectAnswers"][2]}</li>
+                                    <li>{quiz['correctAnswer']}</li>
+                                </ol>
+
+                            </div>
+                            <button type="submit" style={{ diaplay: "flex", float: "right", height: "40px", width: "70px", margin: "auto" }}>Submit</button>
+                            < div>Select Answer:
+                                <select>
+                                    <option value={quiz["incorrectAnswers"][0]}>
+                                        {quiz["incorrectAnswers"][0]}</option>
+                                    <option value={quiz["incorrectAnswers"][1]}>
+                                        {quiz["incorrectAnswers"][1]}</option>
+                                    <option value={quiz["incorrectAnswers"][2]}>
+                                        {quiz["incorrectAnswers"][2]}</option>
+                                    <option value={quiz["correctAnswer"]}>
+                                        {quiz["correctAnswer"]}</option>
+                                </select></div>
+                            < div style={{ color: "#007e3f" }}>Correct Answer
+                            </div>
+
+                        </div >
+                    )
+                })
+            }
         </>
     );
 }
